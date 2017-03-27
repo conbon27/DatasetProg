c65347553e811a60e83ba6f2f660337688eeb6e0
Andrew Conway <a.conway7@nuigalway.ie>
2017-03-27 12:45:55 +0100

    Object initator added to Detail class

--- Detail.cs
+++ Detail.cs
@@ -17,61 +17,88 @@ namespace DatasetProg
             for (var i = 0; i < 33; i++)
             {
                 // populate info with default values for Json-ld files
-                var contact = new ContactPoint();
-                contact.type = "ContactPoint";
-                contact.contactType = "customer service";
-                contact.telephone = "+353 9 138 7200";
-                contact.email = "institute.mail@marine.ie";
+                // using object initiator to create, instantiate & add attributes to each object
+                var contact = new ContactPoint
+                {
+                    type = "ContactPoint",
+                    contactType = "customer service",
+                    telephone = "+353 9 138 7200",
+                    email = "institute.mail@marine.ie"
+                };
 
-                var creator = new Creator();
-                creator.type = "Organization";
-                creator.url = "http://www.marine.ie/Home/";
-                creator.name = "Marine Institute, State agency responsible for marine research, technology development and innovation in Ireland";
-                creator.contactPoint = contact;
+                var creator = new Creator
+                {
+                    type = "Organization",
+                    url = "http://www.marine.ie/Home/",
+                    name =
+                        "Marine Institute, State agency responsible for marine research, technology development and innovation in Ireland",
+                    contactPoint = contact
+                };
 
-                var inc = new IncludedInDataCatalog();
-                inc.type = "DataCatalog";
-                inc.name = "http://data.marine.ie/Category/Index/12";
+                var inc = new IncludedInDataCatalog
+                {
+                    type = "DataCatalog",
+                    name = "http://data.marine.ie/Category/Index/12"
+                };
 
                 // distribution info used to get direct file access
-                var dist1 = new Distribution();
-                dist1.type = "DataDownload";
-                dist1.encodingFormat = "JSON";
-                dist1.contentUrl = results[i].Json; // e.g. "http://erddap.marine.ie/erddap/tabledap/IMI-TidePrediction.json?stationID,time,Water_Level,Water_Level_ODM,longitude,latitude&stationID=%22Achill_Island_MODELLED%22&time%3E=2017-01-01T00:00:00Z&time<=2020-01-01T00:20:00Z&longitude%3E=-10.27019&longitude<=-5.92167&latitude%3E=51.53115&latitude<=55.37168"; // get from DB
+                var dist1 = new Distribution
+                {
+                    type = "DataDownload",
+                    encodingFormat = "JSON",
+                    contentUrl = results[i].Json
+                };
+
+                // e.g. "http://erddap.marine.ie/erddap/tabledap/IMI-TidePrediction.json?stationID,time,Water_Level,Water_Level_ODM,longitude,latitude&stationID=%22Achill_Island_MODELLED%22&time%3E=2017-01-01T00:00:00Z&time<=2020-01-01T00:20:00Z&longitude%3E=-10.27019&longitude<=-5.92167&latitude%3E=51.53115&latitude<=55.37168"; // get from DB
 
 
-                var dist2 = new Distribution();
-                dist2.type = "DataDownload";
-                dist2.encodingFormat = "CSV";
-                dist2.contentUrl = results[i].Csv; // taken from DB
+                var dist2 = new Distribution
+                {
+                    type = "DataDownload",
+                    encodingFormat = "CSV",
+                    contentUrl = results[i].Csv
+                };
+                // taken from DB
 
-                var g = new Geo();
-                g.type = "GeoCoordinates";
-                g.longitude = results[i].Longitude; // e.g. "53.95219" - taken from DB
-                g.latitude = results[i].Latitude; // taken from DB
+                var g = new Geo
+                {
+                    type = "GeoCoordinates",
+                    longitude = results[i].Longitude,
+                    latitude = results[i].Latitude
+                };
+                // e.g. "53.95219" - taken from DB
+                // taken from DB
 
-                var spat = new SpatialCoverage();
-                spat.type = "Place";
-                spat.geo = g;
+                var spat = new SpatialCoverage
+                {
+                    type = "Place",
+                    geo = g
+                };
 
                 // object containing references to other JSON objects
-                var root = new Dataset();
-                root.context = "http://schema.org/";
-                root.type = "Dataset";
-                root.name = "Marine Institute Tide Prediction";
-                root.description = "Tidal predictions are generated from measured data via the Irish National Tide Gauge Network ...";
-                root.url = "http://data.marine.ie/Dataset/Details/20955";
-                root.sameAs = "http://data.marine.ie/Category/Index/12";
+                var root = new Dataset
+                {
+                    context = "http://schema.org/",
+                    type = "Dataset",
+                    name = "Marine Institute Tide Prediction",
+                    description =
+                        "Tidal predictions are generated from measured data via the Irish National Tide Gauge Network ...",
+                    url = "http://data.marine.ie/Dataset/Details/20955",
+                    sameAs = "http://data.marine.ie/Category/Index/12",
+                    keywords = new List<string>()
+                    {
+                        "OCEANOGRAPHY > TIDE > TIDAL WATER",
+                        "OCEANOGRAPHY > TIDE > PREDICTIONS",
+                        "OCEANOGRAPHY > TIDE > " + results[i].StationId
+                    },
+                    includedInDataCatalog = inc,
+                    creator = creator,
+                    distribution = new List<Distribution>() {dist1, dist2},
+                    temporalCoverage = "2017-01-01/2020-01-01",
+                    spatialCoverage = spat
+                };
                 // keywords to be updated by method - pull station_id from DB
-                root.keywords = new List<string>() {"OCEANOGRAPHY > TIDE > TIDAL WATER",
-                "OCEANOGRAPHY > TIDE > PREDICTIONS",
-                "OCEANOGRAPHY > TIDE > "+ results[i].StationId};
-                root.includedInDataCatalog = inc;
-                root.creator = creator;
-                root.distribution = new List<Distribution>() { dist1, dist2 };
                 // important for rest service if used
-                root.temporalCoverage = "2017-01-01/2020-01-01";
-                root.spatialCoverage = spat;
 
                 // check shows data taken in from DB
                 Console.WriteLine("Details Confirmed: " + g.longitude + " " + g.latitude);
